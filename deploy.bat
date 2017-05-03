56188c24a8046a0e21edeb95c5b2286631dc0834
ken39rai <ken39rai@ybb.ne.jp>
2017-05-03 14:48:25 +0900

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 cd "%~pd0"
-copy target\schoo-jenkins.war "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\schoo-jenkins.war"
+copy target\schoo-jenkins.war "C:\Program Files (x86)\Apache Software Foundation\Tomcat 9.0\webapps\schoo-jenkins.war"
